9c0c3ed280e0b2b42dbaa1bb0aa28d345afd9c02
renato1006 <renato1006@gmail.com>
2024-05-15 20:48:42 -0300

    feat: adicionando atributos na classe Abrigo
    Adicionando alguns atributos na classe Abrigo, como os Getters de dois atributos e um List dos Pets cadastrados nesse abrigo

--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -6,12 +6,26 @@ public class Abrigo {
     private String telefone;
     private String email;
 
+    private Pet[] pets;
+
     public Long getId() {
         return id;
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Pet[] getPets() {
+        return pets;
     }
 
     public Abrigo(){}
