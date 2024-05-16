55811d9a7acb57451769541aac6cf3998ddca10a
renato1006 <renato1006@gmail.com>
2024-05-15 21:04:30 -0300

    style: removendo imports não utlizados
    Removendo os imports não utilizados nas classes.

--- src/main/java/br/com/alura/client/ClientHttpConfiguration.java
+++ src/main/java/br/com/alura/client/ClientHttpConfiguration.java
@@ -1,8 +1,6 @@
 package br.com.alura.client;
 
-import br.com.alura.domain.Abrigo;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 
 import java.io.IOException;
 import java.net.URI;
