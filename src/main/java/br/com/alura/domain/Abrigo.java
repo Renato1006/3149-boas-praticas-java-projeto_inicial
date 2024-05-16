ec7bc88d7cc6f4bbd9a05823b265b41d9fe18a05
renato1006 <renato1006@gmail.com>
2024-05-15 21:34:01 -0300

    test: Testando o comportamento de Listar Abrigo
    Realizando o teste de listar os Abrigos. Adicionando um Setter de Id em Abrigo. E reescrevendo o toString da classe para poder realizar o teste.

--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -12,6 +12,10 @@ public class Abrigo {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getNome() {
         return nome;
     }
@@ -34,7 +38,13 @@ public class Abrigo {
         this.nome = nome;
         this.telefone = telefone;
         this.email = email;
-
-
     }
+
+    @Override
+    public String toString() {
+        return """
+                     "id":%s,"nome":"%s","telefone":"%s","email":"%s"
+                     """.formatted(this.id, this.nome, this.telefone, this.email);
+    }
+
 }
